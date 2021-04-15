d425666e402cc7cb9cfe35370169b99186d0fbd1
Jose Bolanos <bolanosjjr@gmail.com>
2021-04-15 13:08:47 -0500

    finished

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,jsonify
+import random
+import requests
 
 app = Flask(__name__)
 
@@ -12,14 +14,43 @@ def homepage():
 @app.route('/api/get-lucky-num', methods=['POST'])
 def lucky_number():
     '''form information entered by a user'''
-    data = request.json
-    user = User(
-        name=data['name'],
-        email=data['email'],
-        year=data['year'],
-        color=data['color'])
+    name = request.json.get('name')
+    email = request.json.get('email')
+    year = request.json.get('year')
+    color = request.json.get('color')
 
-    db.session.add(user)
-    db.session.commit()
+    errors = {}
 
-    return render_template('index.html')
+    if not name:
+        errors['name'] = ['Must enter name']
+
+    if not email:
+        errors['email'] = ['Must enter email']
+
+    if not year:
+        errors['year'] = ['Must enter a 4 digit number']
+
+    if not color:
+        errors['color'] = ['Must enter a color name']
+
+    else:
+        try:
+            year = int(year)
+            if year < 1900 or year > 2000:
+                errors['year'] = ['Must be 4 digit number between 1900 and 2000']
+        except ValueError:
+            errors['year'] = ['Please enter a numeric value']
+    if errors:
+        return jsonify(errors=errors)
+                
+    lucky_nums = {}
+    num = random.randint(1, 100)
+    year_facts = requests.get(f'http://numbersapi.com/{year}/year')
+    year_text = year_facts.text
+    num_facts = requests.get(f'http://numbersapi.com/{num}')
+    num_text = num_facts.text
+    lucky_nums['year'] = {'fact': year_text, 'year': year}
+    lucky_nums['num'] = {'fact':num_text,'num':num}
+
+        
+    return jsonify(lucky_nums)
--- lucky-nums/static/lucky.js
+++ lucky-nums/static/lucky.js
@@ -1,11 +1,58 @@
+const $form = $('#lucky-form');
+const $nameErr = $("#name-err");
+const $name = $("#name");
+const $yearErr = $("#year-err");
+const $year = $("#year");
+const $emailErr = $("#email-err");
+const $email = $("#email");
+const $colorErr = $("#color-err")
+const $color = $("#color");
+const $results = $("#lucky-results")
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+async function processForm(evt) {
+    evt.preventDefault();
+    data = {
+		name  : $name.val(),
+		year  : $year.val(),
+		email : $email.val(),
+		color : $color.val()
+	};
+
+	// ping the back-end API with the form data
+	let response = await axios.post('/api/get-lucky-num', data);
+
+	// run the handleResponse function to manipulate dom
+	handleResponse(response);
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
-function handleResponse(resp) {
+function handleResponse(response) {
+    let text = response.data;
+	$('b').html('');
+	$results.text('');
+
+	if (text.errors) {
+        
+        const errors = text.errors;
+        for (let err_list in errors) {
+            for (let err of errors[err_list]) {
+                $(`#${err_list}-err`).append(err);
+            }
+        }
+       
+    } else {
+       
+        let numberData = text.num;
+        let yearData = text.year;
+
+        let numberFact = `Your lucky number is ${numberData.num} (${numberData.fact})`;
+        let yearFact = `Your birth year, ${yearData.year}, fact is: ${yearData.fact}`;
+
+        
+        $results.append(numberFact).append(yearFact);
+    }
 }
 
 
